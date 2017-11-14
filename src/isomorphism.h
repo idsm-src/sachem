4df2ac3011311303b53666269613d73d199c926f
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2017-11-14 11:02:45 +0100

    Add the copyright to isomorphism.h

--- src/isomorphism.h
+++ src/isomorphism.h
@@ -1,3 +1,32 @@
+/*
+ * Copyright (C) 2001
+ *   Dipartimento di Informatica e Sistemistica,
+ *   Universita degli studi di Napoli ``Federico II'
+ *   <http://amalfi.dis.unina.it>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ *  1. The above copyright notice and this permission notice shall be included in all copies or substantial
+ *      portions of the Software, together with the associated disclaimers.
+ *  2. Any modification to the standard distribution of the Software shall be mentioned in a prominent notice
+ *      in the documentation provided with the modified distribution, stating clearly how, when and by
+ *      whom the Software has been modified.
+ *  3. Either the modified distribution shall contain the entire sourcecode of the standard distribution of the
+ *      Software, or the documentation shall provide instructions on where the source code of the standard
+ *      distribution of the Software can be obtained.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
 #ifndef ISOMORPHISM_H__
 #define ISOMORPHISM_H__
 
