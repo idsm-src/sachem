939803bfb4b3d39a38e75597d954a8d26dd1f917
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2017-11-26 15:24:02 +0100

    Add support for the query cancel request in the isomorphism module

--- src/isomorphism.h
+++ src/isomorphism.h
@@ -686,6 +686,9 @@ inline bool vf2state_match_core(VF2State *const restrict vf2state)
 
         while(vf2state_next_target(vf2state))
         {
+            if(unlikely(QueryCancelPending))
+                return false;
+
 #if USE_VF2_TIMEOUT
             if(unlikely(vf2Timeouted))
                 return false;
