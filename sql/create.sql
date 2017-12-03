d9ae806bf4384f9ef9d8eed1ffdb384cb561102f
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2017-12-03 14:44:21 +0100

    Add triggers to audit changes in the compounds table

--- sql/create.sql
+++ sql/create.sql
@@ -4,6 +4,12 @@ CREATE TABLE compounds (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE orchem_compound_audit (
+    id                    INT NOT NULL,
+    stored                BOOLEAN NOT NULL,
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE orchem_molecules (
     seqid                 INT NOT NULL,
     id                    INT NOT NULL,
@@ -37,3 +43,29 @@ CREATE FUNCTION "orchem_substructure_write_indexes"() RETURNS bool AS 'libsachem
 CREATE FUNCTION "orchem_load_data"() RETURNS int AS 'libsachem.so','orchem_load_data' LANGUAGE C IMMUTABLE STRICT;
 CREATE FUNCTION "lucy_substructure_create_index"() RETURNS bool AS 'libsachem.so','lucy_substructure_create_index' LANGUAGE C IMMUTABLE STRICT;
 CREATE FUNCTION "lucy_substructure_optimize_index"() RETURNS bool AS 'libsachem.so','lucy_substructure_optimize_index' LANGUAGE C IMMUTABLE STRICT;
+
+
+CREATE FUNCTION orchem_compound_audit() RETURNS TRIGGER AS
+$body$
+BEGIN
+  IF (TG_OP = 'UPDATE' OR TG_OP = 'INSERT') THEN
+    INSERT INTO orchem_compound_audit (id, stored) VALUES (NEW.id, true)
+        ON CONFLICT (id) DO UPDATE SET id=EXCLUDED.id, stored=true;
+    RETURN NEW;
+  ELSIF (TG_OP = 'DELETE') THEN
+    INSERT INTO orchem_compound_audit (id, stored) VALUES (OLD.id, false)
+        ON CONFLICT (id) DO UPDATE SET id=EXCLUDED.id, stored=false;
+    RETURN OLD;
+  ELSIF (TG_OP = 'TRUNCATE') THEN
+    INSERT INTO orchem_compound_audit SELECT id, false FROM compounds
+        ON CONFLICT (id) DO UPDATE SET id=EXCLUDED.id, stored=false;
+    RETURN NULL;
+  END IF;
+END;
+$body$ LANGUAGE plpgsql;
+
+CREATE TRIGGER orchem_compound_audit BEFORE INSERT OR UPDATE OR DELETE ON compounds
+    FOR EACH ROW EXECUTE PROCEDURE orchem_compound_audit();
+
+CREATE TRIGGER orchem_truncate_compounds_audit BEFORE TRUNCATE ON compounds
+    FOR EACH STATEMENT EXECUTE PROCEDURE orchem_compound_audit();
