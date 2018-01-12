445ec9721f43c4efe0cfce70d8604a6997a4c7ae
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2018-01-12 11:36:35 +0100

    Fix orchem_substructure_search to handle empty index records correctly

--- src/subsearch.c
+++ src/subsearch.c
@@ -353,7 +353,7 @@ Datum orchem_substructure_search(PG_FUNCTION_ARGS)
                 if(unlikely(SPI_execute_plan(mainQueryPlan, values, NULL, true, 0) != SPI_OK_SELECT))
                     elog(ERROR, "%s: SPI_execute_plan() failed", __func__);
 
-                if(unlikely(SPI_processed != count || SPI_tuptable == NULL || SPI_tuptable->tupdesc->natts != 4))
+                if(unlikely(/*SPI_processed != count ||*/ SPI_tuptable == NULL || SPI_tuptable->tupdesc->natts != 4))
                     elog(ERROR, "%s: SPI_execute_plan() failed", __func__);
 
                 info->table = SPI_tuptable;
