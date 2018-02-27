c6a5e20240e8fcd7c8397dc84d3a43e86cc36b7c
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2018-02-27 15:54:03 +0100

    Make common declarations supporting java calls external

--- src/java.c
+++ src/java.c
@@ -12,55 +12,11 @@
 #include "bitset.h"
 
 
-#define JavaDeleteRef(ref) \
-    do \
-    { \
-        if(likely(ref != NULL)) \
-        (*env)->DeleteLocalRef(env, ref); \
-        ref = NULL; \
-    } while(0)
-
-
-#define JavaDeleteBooleanArray(array, buffer, mode) \
-    do { \
-        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseBooleanArrayElements(env, array, buffer, mode); \
-        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
-        array = NULL; \
-        buffer = NULL; \
-    } while(0)
-
-
-#define JavaDeleteByteArray(array, buffer, mode) \
-    do { \
-        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseByteArrayElements(env, array, buffer, mode); \
-        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
-        array = NULL; \
-        buffer = NULL; \
-    } while(0)
-
-
-#define JavaDeleteShortArray(array, buffer, mode) \
-    do { \
-        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseShortArrayElements(env, array, buffer, mode); \
-        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
-        array = NULL; \
-        buffer = NULL; \
-    } while(0)
-
-
-#define JavaDeleteLongArray(array, buffer, mode) \
-    do { \
-        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseLongArrayElements(env, array, buffer, mode); \
-        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
-        array = NULL; \
-        buffer = NULL; \
-    } while(0)
-
+JavaVM* jvm = NULL;
+JNIEnv* env = NULL;
 
 static bool initialised = false;
 static pthread_t mainThread;
-static JavaVM* jvm = NULL;
-static JNIEnv* env = NULL;
 static jclass exceptionClass = NULL;
 static jclass byteArrayClass = NULL;
 static jmethodID toStringMethod = NULL;
@@ -95,7 +51,7 @@ static jfieldID lucyLoaderMoleculeField = NULL;
 static jmethodID lucyLoaderDataMethod = NULL;
 
 
-static inline void java_check_exception(const char *str)
+void java_check_exception(const char *str)
 {
     jthrowable exception = (*env)->ExceptionOccurred(env);
 
--- src/java.h
+++ src/java.h
@@ -3,8 +3,64 @@
 
 #include <postgres.h>
 #include <utils/array.h>
+#include <jni.h>
 #include <stdbool.h>
 #include <stdint.h>
+
+
+#define JavaDeleteRef(ref) \
+    do \
+    { \
+        if(likely(ref != NULL)) \
+        (*env)->DeleteLocalRef(env, ref); \
+        ref = NULL; \
+    } while(0)
+
+
+#define JavaDeleteBooleanArray(array, buffer, mode) \
+    do { \
+        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseBooleanArrayElements(env, array, buffer, mode); \
+        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
+        array = NULL; \
+        buffer = NULL; \
+    } while(0)
+
+
+#define JavaDeleteByteArray(array, buffer, mode) \
+    do { \
+        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseByteArrayElements(env, array, buffer, mode); \
+        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
+        array = NULL; \
+        buffer = NULL; \
+    } while(0)
+
+
+#define JavaDeleteShortArray(array, buffer, mode) \
+    do { \
+        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseShortArrayElements(env, array, buffer, mode); \
+        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
+        array = NULL; \
+        buffer = NULL; \
+    } while(0)
+
+
+#define JavaDeleteIntegerArray(array, buffer, mode) \
+    do { \
+        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseIntArrayElements(env, array, buffer, mode); \
+        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
+        array = NULL; \
+        buffer = NULL; \
+    } while(0)
+
+
+#define JavaDeleteLongArray(array, buffer, mode) \
+    do { \
+        if(likely(array != NULL && buffer != NULL)) (*env)->ReleaseLongArrayElements(env, array, buffer, mode); \
+        if(likely(array != NULL)) (*env)->DeleteLocalRef(env, array); \
+        array = NULL; \
+        buffer = NULL; \
+    } while(0)
+
 
 
 typedef struct
@@ -42,6 +98,11 @@ typedef struct
 } LucyLoaderData;
 
 
+extern JavaVM* jvm;
+extern JNIEnv* env;
+
+
+void java_check_exception(const char *str);
 int java_parse_substructure_query(SubstructureQueryData **data, char* query, size_t queryLength, int32_t type, bool implicitHydrogens, bool tautomers);
 int java_parse_orchem_substructure_query(OrchemSubstructureQueryData **data, char* query, size_t queryLength, int32_t type, bool implicitHydrogens, bool tautomers);
 int java_parse_orchem_similarity_query(uint64_t **data, char* query, size_t queryLength, int32_t type);
