1288a3e563144f4d0a735ec2e83ae5cc0963e2ba
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2018-03-03 13:46:39 +0100

    Reorganize the code of the lucy extension

---
+++ src/lucy/common.h
@@ -0,0 +1,11 @@
+#ifndef LUCY_COMMON_H_
+#define LUCY_COMMON_H_
+
+
+#define COMPOUNDS_TABLE         "compounds"
+#define MOLECULES_TABLE         "sachem_molecules"
+#define MOLECULE_ERRORS_TABLE   "sachem_molecule_errors"
+#define AUDIT_TABLE             "sachem_compound_audit"
+#define INDEX_TABLE             "sachem_index"
+
+#endif /*LUCY_COMMON_H_*/
