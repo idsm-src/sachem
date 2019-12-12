9857bb72d5668f71427296074a60764cd9a6d874
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2019-12-12 13:08:18 +0100

    Change behavior to index all molecules

--- java/src/cz/iocb/sachem/search/LucyLoader.java
+++ java/src/cz/iocb/sachem/search/LucyLoader.java
@@ -2,12 +2,7 @@ package cz.iocb.sachem.search;
 
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.atomic.AtomicInteger;
-import org.openscience.cdk.exception.CDKException;
-import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IBond;
-import org.openscience.cdk.interfaces.IPseudoAtom;
-import org.openscience.cdk.isomorphism.matchers.QueryBond;
 import cz.iocb.sachem.isomorphism.IsomorphismSort;
 import cz.iocb.sachem.shared.MoleculeCreator;
 
@@ -48,9 +43,6 @@ public class LucyLoader
                             IAtomContainer readMolecule = MoleculeCreator.getMoleculeFromMolfile(molfile);
                             MoleculeCreator.configureMolecule(readMolecule);
 
-                            if(!canBeIndexed(readMolecule))
-                                throw new CDKException("query molecule cannot be indexed");
-
                             // calculate molecule binary representation
                             readMolecule.setAtoms(IsomorphismSort.atomsByFrequency(readMolecule));
                             SachemMoleculeBuilder builder = new SachemMoleculeBuilder(readMolecule);
@@ -75,19 +67,5 @@ public class LucyLoader
             thread[c].join();
 
         return result;
-    }
-
-
-    private static boolean canBeIndexed(IAtomContainer molecule)
-    {
-        for(IAtom atom : molecule.atoms())
-            if(atom instanceof IPseudoAtom)
-                return false;
-
-        for(IBond bond : molecule.bonds())
-            if(bond instanceof QueryBond && !bond.isAromatic())
-                return false;
-
-        return true;
     }
 }
